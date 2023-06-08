07e1e2631d72f9e28b15ea342bd60fd2660d5c6a
unalysuf <unalysuf@gmail.com>
2023-06-08 15:58:42 +0300

    Movie Backend Done

---
+++ src/main/java/com/yusuf/movies/MoviesApplication.java
@@ -0,0 +1,18 @@
+package com.yusuf.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+
+
+}
